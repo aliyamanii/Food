5ab93fb967996cb392a60549561057a6c1b228ee
Ali Yamani <aliyamanii@outlook.com>
2023-11-18 02:38:48 +0330

    [ add ] sample card-paragraph

--- public/index.js
+++ public/index.js
@@ -2,5 +2,5 @@ const navLinks = document.querySelector(".nav-links");
 
 function onToggleMenu(e) {
   e.name = e.name === "menu" ? "close" : "menu";
-  navLinks.classList.toggle("top-[5.5%]");
+  navLinks.classList.toggle("top-[90%]");
 }
